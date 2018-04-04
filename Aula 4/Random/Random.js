c52291356cb7e51458cd0368ac3583a5cc8e556c
Nuno837 <nunofilmaia@yahoo.com.br>
2018-04-04 15:47:02 +0100

    Aula 4
    Random

---
+++ Aula 4/Random/Random.js
@@ -0,0 +1,58 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+document.getElementById("a").addEventListener("click", function(e){
+		var rndCol = 'rgb(' + Math.floor(1 + Math.random() * 255) + ',' + Math.floor(1 + Math.random() * 255) + ',' + Math.floor(1 + Math.random() * 255) + ')';
+    e.target.style.backgroundColor = rndCol;
+}); 
+
+document.getElementById("a").addEventListener("click", function(e){
+		var rndCol = 'rgb(' + Math.floor(1 + Math.random() * 255) + ',' + Math.floor(1 + Math.random() * 255) + ',' + Math.floor(1 + Math.random() * 255) + ')';
+    e.target.style.backgroundColor = rndCol;
+}); 
+
+document.getElementById("a").addEventListener("click", function(e){
+		var rndCol = 'rgb(' + Math.floor(1 + Math.random() * 255) + ',' + Math.floor(1 + Math.random() * 255) + ',' + Math.floor(1 + Math.random() * 255) + ')';
+    e.target.style.backgroundColor = rndCol;
+}); 
+
+document.getElementById("b").addEventListener("click", function(e){
+		var rndCol = 'rgb(' + Math.floor(1 + Math.random() * 255) + ',' + Math.floor(1 + Math.random() * 255) + ',' + Math.floor(1 + Math.random() * 255) + ')';
+    e.target.style.backgroundColor = rndCol;
+}); 
+
+document.getElementById("c").addEventListener("click", function(e){
+		var rndCol = 'rgb(' + Math.floor(1 + Math.random() * 255) + ',' + Math.floor(1 + Math.random() * 255) + ',' + Math.floor(1 + Math.random() * 255) + ')';
+    e.target.style.backgroundColor = rndCol;
+}); 
+
+document.getElementById("d").addEventListener("click", function(e){
+		var rndCol = 'rgb(' + Math.floor(1 + Math.random() * 255) + ',' + Math.floor(1 + Math.random() * 255) + ',' + Math.floor(1 + Math.random() * 255) + ')';
+    e.target.style.backgroundColor = rndCol;
+}); 
+
+document.getElementById("e").addEventListener("click", function(e){
+		var rndCol = 'rgb(' + Math.floor(1 + Math.random() * 255) + ',' + Math.floor(1 + Math.random() * 255) + ',' + Math.floor(1 + Math.random() * 255) + ')';
+    e.target.style.backgroundColor = rndCol;
+}); 
+
+document.getElementById("f").addEventListener("click", function(e){
+		var rndCol = 'rgb(' + Math.floor(1 + Math.random() * 255) + ',' + Math.floor(1 + Math.random() * 255) + ',' + Math.floor(1 + Math.random() * 255) + ')';
+    e.target.style.backgroundColor = rndCol;
+}); 
+
+document.getElementById("g").addEventListener("click", function(e){
+		var rndCol = 'rgb(' + Math.floor(1 + Math.random() * 255) + ',' + Math.floor(1 + Math.random() * 255) + ',' + Math.floor(1 + Math.random() * 255) + ')';
+    e.target.style.backgroundColor = rndCol;
+}); 
+
+document.getElementById("h").addEventListener("click", function(e){
+		var rndCol = 'rgb(' + Math.floor(1 + Math.random() * 255) + ',' + Math.floor(1 + Math.random() * 255) + ',' + Math.floor(1 + Math.random() * 255) + ')';
+    e.target.style.backgroundColor = rndCol;
+}); 
+
+
