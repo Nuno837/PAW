4ac1a3d61d48541645af38dc4a8223f2e9e10c56
Nuno837 <nunofilmaia@yahoo.com.br>
2018-04-11 15:00:00 +0100

    Iterators

---
+++ Aula 4/Iterators/forEach.js
@@ -0,0 +1,11 @@
+let fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+
+// Iterate over fruits below
+
+fruits.forEach(function(fruitItem) {
+              console.log('I want to eat a' + fruitItem);
+              });
+
+console.log('\n');
+
+fruits.forEach(fruitItem => console.log('I want to eat ' + fruitItem));
